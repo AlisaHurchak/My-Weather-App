4abc733cb75e94062d240b3e2b7e6fb26da253cf
Alisa Hurchak <alisahurchak@gmail.com>
2024-08-24 17:59:28 +0100

    Added Search Functions using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+function displayWeatherData(response) {
+  let temperatureElement = document.querySelector("#weather-app-current-temp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-search-input");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "cbc74oa354dba2dbcf0te4ef1b7eef0f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherData);
+}
+
 function handleSearchForm(event) {
   event.preventDefault();
   let searchData = document.querySelector("#search-form");
-  let cityElement = document.querySelector("#city-search-input");
-  cityElement.innerHTML = searchData.value;
+  searchCity(searchData.value);
 }
 
 let searchInputElement = document.querySelector("#search-input");
 searchInputElement.addEventListener("submit", handleSearchForm);
+
+searchCity("Malaga");
